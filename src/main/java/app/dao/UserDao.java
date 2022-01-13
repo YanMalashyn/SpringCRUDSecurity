a3d742ca642b976680f68e9e82cfca309ba408bd
Yan.Malashyn <lamantinspb1@gmail.com>
2022-01-13 23:31:08 +0300

    add xz

---
+++ src/main/java/app/dao/UserDao.java
@@ -0,0 +1,11 @@
+package app.dao;
+
+import app.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+    List<User> listUsers();
+    User findUserByCar(String model, int series);
+}
